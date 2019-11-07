d3aff7f5f5e40ea1e134c075b6d47c934ef6664c
Mikhail Yudin <yumih@r-o.ru>
2019-11-07 20:17:00 +0700

    inventory slots positioning, window resize behavior fixed

--- js/main.js
+++ js/main.js
@@ -3,24 +3,31 @@ const app = new PIXI.Application({
   resolution: devicePixelRatio
 })
 document.body.appendChild(app.view)
+app.renderer.view.style.position = 'absolute'
+app.renderer.view.style.display = 'block'
+app.renderer.autoDensity = true
 
 let loader = PIXI.Loader.shared
 loader.add('sheet', 'assets/ss.json')
 loader.load(init)
 
+let inventory = new Inventory(8)
+inventory.pivot.x = inventory.width / 2
+inventory.pivot.y = inventory.height
+app.stage.addChild(inventory)
+
 function init () {
   let sheet = loader.resources.sheet
   let spr = new PIXI.Sprite(sheet.textures['bricks'])
-  const inventory = new Inventory(8)
-
   app.stage.addChild(spr)
-  app.stage.addChild(inventory)
 }
 
 window.addEventListener('resize', resize)
 
 function resize () {
   app.renderer.resize(window.innerWidth, window.innerHeight)
+  inventory.x = app.renderer.view.width / 2
+  inventory.y = app.renderer.view.height
 }
 
 resize()
